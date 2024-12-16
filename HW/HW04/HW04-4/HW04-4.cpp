84fe9b755f40e1ccb6ed4c01d484e4201c869481
boonyakorn <boonyakornboontang@gmail.com>
2024-12-17 04:25:56 +0700

    Add files via upload

---
+++ HW/HW04/HW04-4/HW04-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    float h, w ;
+
+    printf( "Enter H & W = " ) ;
+    scanf( "%f %f", &h, &w ) ;
+    printf( "Triagle Area = %.1f", ( h * w ) / 2 ) ;
+
+    return 0 ;
+}//end main
