63463b34f07d98b0dfdcdc77779f32ecd3a55e30
boonyakorn <boonyakornboontang@gmail.com>
2025-03-06 18:52:11 +0700

    Update HW06-5.cpp

--- HW/HW06/HW06-5/HW06-5.cpp
+++ HW/HW06/HW06-5/HW06-5.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
-int main () {
 
+int main() {
     int i, n, b ;
-    printf( "Enter number : " ) ;
+    printf( "Enter number: " ) ;
     scanf( "%d", &i ) ;
-    
-    for( n = 2 ; n <= i ; n ++ ) {
+
+    for( n = i ; n >= 2; n-- ) { 
         b = 2 ;
-        for( ; b < n ; b ++ ) {
-            if ( n % b == 0 ) {
+        for( ; b < n ; b++ ) {
+            if( n % b == 0 ) {
                 break ;
-            }//end for
+            }//end if
         }//end for
-        if( n == b ){
+        if( n == b ) {
             printf( "%d ", n ) ;
         }//end if
     }//end for
